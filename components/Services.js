b669c7477ea94f9b58e336f564bea9f85da5ea06
Faan02 <rudfanno1@gmail.com>
2022-06-28 01:39:00 +0700

    style service list

--- components/Services.js
+++ components/Services.js
@@ -10,43 +10,85 @@ const useStyles = makeStyles({
     postcardImg: {
         width:"300px",
     },
+    serviceIcon:{
+        width:"100px",
+        height:"100px"
+    }
     
 }, { name: "MuiExample_Component" })
 
-const GetstartButton = styled(Button)(({ theme }) => ({
-    borderRadius: "10px",
-    padding: "10px 14px",
-    backgroundColor:"#47B5FF",
-    boxShadow: "none",
-    color: "white",
-    fontWeight:"600",
+const ServiceBox = styled(Box)(({ theme }) => ({
+    width:"100%",
+    height:"95%",
+    border:"3px solid #FFE3A9",
+    borderRadius:"10px",
+    padding:"10px 50px",
+    transitionDuration:"0.5s",
     "&:hover": {
-        backgroundColor: "#0AA1DD",
-        boxShadow: "none",
+        backgroundColor: "#FFE3A9",
+        height:"100%",
+        padding:"20px 50px 5px 50px",
+    },
+    "&:hover #serviceButton":{
+        backgroundColor:"#47B5FF",
+        color:"white"
     }
+    
 }))
-const SeeServiceButton = styled(Button)(({ theme }) => ({
+
+const GetStartServiceButton = styled(Button)(({ theme }) => ({
+    transitionDuration:"0.5s",
     borderRadius: "10px",
     padding: "10px 14px",
-    border:"2px solid white",
+    width:"150px",
+    border:"2px solid #47B5FF",
     backgroundColor:"rgba(0,0,0,0)",
     boxShadow: "none",
-    color: "white",
+    color: "#47B5FF",
     fontWeight:"600",
-    "&:hover": {
-        backgroundColor: "#47B5FF",
-        boxShadow: "none",
-    
-    }
+    marginTop:"7px",
+
 }))
 
 export default function Services() {
     const classes = useStyles();
     return (
-        <Grid container sx={{ width: "100%", height: "45vh" ,marginTop:"50px",padding: "0px calc((100vw - 984px) / 2)",textAlign:"center"}}>
+        <Grid container sx={{ width: "100%", height: "100%" ,marginTop:"50px",padding: "0px calc((100vw - 984px) / 2)",textAlign:"center"}}>
             <Grid item lg={12}><Typography variant="h5" sx={{fontWeight:"600",color:"#112B3C"}}>OUR SERVICES</Typography></Grid>
-            <Grid item lg={6} sx={{marginTop:"10px"}}>HELLO</Grid>
-            <Grid item lg={6} sx={{marginTop:"10px"}}>HELLO</Grid>
+            <Grid container sx={{height:"300px"}}>
+                <Grid item lg={4} sx={{padding:"10px 7px"}}>
+                    <ServiceBox>
+                        <img src="https://cdn-icons-png.flaticon.com/512/3656/3656940.png" className={classes.serviceIcon}/>
+                        <Typography variant="h6" sx={{fontWeight:"600",fontSize:"24px"}}>Wedding Wish</Typography>
+                        <Typography variant="a" sx={{fontSize:"16px"}}>Make your wedding easier with this service</Typography>
+                        <GetStartServiceButton id="serviceButton" variant="contained">GET STARTED</GetStartServiceButton>
+                    </ServiceBox>
+                </Grid>
+                <Grid item lg={4} sx={{padding:"10px 7px"}}>
+                    <ServiceBox>
+                        <img src="https://cdn-icons-png.flaticon.com/512/3656/3656940.png" className={classes.serviceIcon}/>
+                        <Typography variant="h6" sx={{fontWeight:"600",fontSize:"24px"}}>Wedding Wish</Typography>
+                        <Typography variant="a" sx={{fontSize:"16px"}}>Make your wedding easier with this service</Typography>
+                        <GetStartServiceButton id="serviceButton" variant="contained">GET STARTED</GetStartServiceButton>
+                    </ServiceBox>
+                </Grid>
+                <Grid item lg={4} sx={{padding:"10px 7px"}}>
+                    <ServiceBox>
+                        <img src="https://cdn-icons-png.flaticon.com/512/3656/3656940.png" className={classes.serviceIcon}/>
+                        <Typography variant="h6" sx={{fontWeight:"600",fontSize:"24px"}}>Wedding Wish</Typography>
+                        <Typography variant="a" sx={{fontSize:"16px"}}>Make your wedding easier with this service</Typography>
+                        <GetStartServiceButton id="serviceButton" variant="contained">GET STARTED</GetStartServiceButton>
+                    </ServiceBox>
+                </Grid>
+            </Grid>
+            <Grid container sx={{height:"300px",textAlign:"center",display:"flex",justifyContent:"center",marginLeft:"auto",marginRight:"auto"}}>
+                <Grid item lg={4} sx={{padding:"10px 7px"}}>
+                    <Box sx={{width:"100%",height:"100%",background:"linear-gradient(to top, #9eb23b 5%, #c7d36f 100%)"}}></Box>
+                </Grid>
+                <Grid item lg={4} sx={{padding:"10px 7px"}}>
+                    <Box sx={{width:"100%",height:"100%",backgroundColor:"red"}}></Box>
+                </Grid>
+            </Grid>
         </Grid>
     )
 }
