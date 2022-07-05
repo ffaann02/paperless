a75aafe1a0c6b2725453164a592a78bda5a03e03
Faan02 <rudfanno1@gmail.com>
2022-07-05 15:19:30 +0700

    responsive register page

--- pages/login.js
+++ pages/login.js
@@ -32,11 +32,8 @@ const useStyles = makeStyles({
     },
     registerBanner:{
         width:"100%",
-        marginTop:"150px",
-        ['@media (max-width:900px)']:{
-            marginTop:"50px"
-        }
-    },
+        marginTop:"150px"
+    }
 }, { name: "MuiExample_Component" })
 
 
@@ -57,34 +54,31 @@ const SubmitRegisterButton = styled(Button)(({ theme }) => ({
     }
 }))
 
-export default function Register() {
+export default function Login() {
     const classes = useStyles();
     return (
-        <Grid container sx={{ width: "100%",height: {lg:"100vh",md:"100vh",sm:"100%"},textAlign:"center"}}>
-            <Grid item lg={12} md={12} sm={12} xs={12} sx={{padding: {lg:"calc((100vh - 600px) / 2) calc((100vw - 984px) / 2)"
-            ,md:"calc((100vh - 600px) / 2) calc((100vw - 984px) / 2)",sm:"60px",xs:"40px"}}}>
+        <Grid container sx={{ width: "100%",height: {lg:"80vh",md:"90vh",sm:"100%"},marginTop:"60px",textAlign:"center"}}>
+            <Grid item lg={12} md={12} sm={12} xs={12} sx={{padding: {lg:"calc((100vh - 984px) / 2) calc((100vw - 984px) / 2)"
+            ,md:"calc((100vh - 984px) / 2) calc((100vw - 984px) / 2)",sm:"20px",xs:"40px"}}}>
                 <Grid container sx={{width:"100%",height:"100%",boxShadow:"rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",borderRadius:"20px"}}>
-                    <Grid order={{lg:1,md:1,sm:2,xs:2}} item lg={6} md={6} sm={12} xs={12} sx={{backgroundColor:"#C0DFD9",borderRadius:{lg:"20px 0 0 20px",md:"20px 0 0 20px"
-                    ,sm:"0 0 20px 20px",xs:"0 0 20px 20px"}}}>
+                    <Grid item lg={6} md={6} sx={{backgroundColor:"#C0DFD9",borderRadius:"20px 0 0 20px"}}>
                         <img src="/registerBanner.svg" className={classes.registerBanner}/>
                         <Typography variant="h6" sx={{fontSize:"24px",fontWeight:"600",padding:"0 40px",marginTop:"20px"}}>
                             ONLINE WISHES & INVITATIONS FOR FREE
                         </Typography>
                     </Grid>
-                    <Grid order={{lg:2,md:2,sm:1,xs:1}} item lg={6} md={6} sm={12} xs={12} sx={{borderRadius:"0 20px 20px 0",padding:"40px 60px"}}>
+                    <Grid item lg={6} md={6} sx={{borderRadius:"0 20px 20px 0",padding:"60px 60px 40px 60px"}}>
                         <Typography variant="h6" sx={{color:"white",padding:"0"}}>
                             <Typography variant="a" sx={{color:"#EC9C04",fontSize:"30px",fontWeight:"600"}}>PAPER</Typography>
                             <Typography variant="a" sx={{color:"#5bccf6",fontSize:"30px",fontWeight:"600"}}>LESS</Typography>
                         </Typography>
                         <Box sx={{width:"100%",marginTop:"20px"}}>
-                            <TextField id="username" label="Username" variant="standard" type="text" sx={{width:"100%",marginTop:"10px"}}/>
-                            <TextField id="password" label="Password" variant="standard" type="password" sx={{width:"100%",marginTop:"10px"}}/>
-                            <TextField id="confirm_password" label="Confirm Password" variant="standard" type="password" sx={{width:"100%",marginTop:"10px"}}/>
-                            <TextField id="email" label="Email" variant="standard" type="email" sx={{width:"100%",marginTop:"10px"}}/>
+                            <TextField id="username" label="Username" variant="standard" type="text" sx={{width:"100%",marginTop:"20px"}}/>
+                            <TextField id="password" label="Password" variant="standard" type="password" sx={{width:"100%",marginTop:"20px"}}/>
                             <Typography variant="h6" sx={{fontSize:"14px",marginTop:"20px"}}>
-                                Already a member ? <Typography variant="a" sx={{fontWeight:"600",cursor:"pointer",fontSize:"16px"}}>Log In</Typography>
+                                Don't have an account <Typography variant="a" sx={{fontWeight:"600",cursor:"pointer",fontSize:"16px"}}>Sign Up</Typography>
                             </Typography>
-                            <SubmitRegisterButton variant="contained">SIGN UP</SubmitRegisterButton>
+                            <SubmitRegisterButton variant="contained">LOG IN</SubmitRegisterButton>
                         </Box>
                     </Grid>
                 </Grid>
